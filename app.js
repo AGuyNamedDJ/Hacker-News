36793197fc510c69536e2079a301265c2cbc340e
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-26 17:39:42 -0500

    Project complete

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ const app = express();
     //Optional combine Steps 1 & 2
     // const app = require("express")();
 
+// Step 6: Boot up Express server by "listening" to itconst PORT = 3000;
+//Port Variable
+const PORT = 3000;
+
 // Step 3: Bring in Morgan Middleware
 app.use(morgan("dev"));
+
 
 // Step 4a: Express.Static
 const staticMiddleware = express.static(path.join(__dirname, "public"));
@@ -24,54 +29,92 @@ const staticMiddleware = express.static(path.join(__dirname, "public"));
 // Setup 4b: Middleware Browser Handeler
 app.use(staticMiddleware);
  
-
 // Step 5: Write 1st Route Handler
 app.get("/", (req, res) => {
     // home path ./
 
     // posts = the data from the postBank.js we grabbed
-    const posts = postBank.list();
+  const posts = postBank.list();
 
-    // data for actual page content, update after making
-    const html = `<!DOCTYPE html>
-    <html>
-    <head>
-        <title>Wizard News</title>
-        <link rel="stylesheet" href="/style.css" />
-    </head>
+  // data for actual page content, update after making
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
 
-    <html>
-    <body>
+  <body>
     <div class="news-list">
-    <header><img src="/logo.png"/>Wizard News</header>
-    ${posts.map(post => `
-    <div class='news-item'>
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position"> <a href="/posts/${post.id}"> ▲${post.title}</a>
+           
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
+    // Here we have tagged the style.css in the header 
+
+    // Sending out the response
+  res.send(html);
+});
+
+
+app.get('/posts/:id', (req, res)=> { 
+const id = req.params.id;
+const post = postBank.find(id);
+const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class='news-item'>
       <p>
         <span class="news-position">${post.id}. ▲</span>${post.title}
         <small>(by ${post.name})</small>
       </p>
-      <small class="news-info">
-        ${post.upvotes} upvotes | ${post.date}
-      </small>
-    </div>`
-  ).join('')}
-</div>
-</body>
-</html>`
-    // Here we have tagged the style.css in the header 
-
-// Sending out the response
-    res.send(html);
-});
+      <p class="news-info">
+        ${post.content}  | ${post.date}
+      </p>
+    </div>
+  </body>
+</html>`;
 
 
-// Step 6: Boot up Express server by "listening" to itconst PORT = 3000;
-//Port Variable
-const PORT = 3000;
+if(!post.id) {   
+  throw new Error('Not Found')
+} else {
+
+
+
+
+res.send(html)
+}})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Oops, Page Not Found')
+})
 
 // Server listening
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+  console.log(`App listening in port ${PORT}`);
 });
 
 // Understand what the ${post} . map is doing; review
