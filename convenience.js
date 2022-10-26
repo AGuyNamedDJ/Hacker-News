5871823f921164f9329eb9ea9bd149c655b315da
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-26 14:11:37 -0500

    postBank file created

---
+++ convenience.js
@@ -0,0 +1,14 @@
+// Creating a Modular & Mountable Mini Express Router
+
+// Step 1: Import Express
+const express = require ("express");
+
+// Step 2: Use Built In Express . Router Middleware
+const convenienceRouter = express.Router();
+
+// Step 3: Bring in all of the Route Handlers
+    // here
+
+
+// Step 4: Export Mini Router
+module.exports = convenienceRouter;
