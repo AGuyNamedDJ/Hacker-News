40076891212ed29d96dfa8b5ffbe9378ceafc90e
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-26 11:38:04 -0500

    updates to app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Step 1: Node Runtime Env Import
 const express = require("express");
+const morgan = require("morgan");
+const path = 
 
 // Browser Runtime Env Import
 // import React from "react";
@@ -15,13 +17,25 @@ app.use(morgan("dev"));
 
 // Step 4: Express.Static
 
+
 // Step 5: Write 1st Route Handler
 app.get("/", (req, res, next) => {
     console.log("Bonjou!")
-    res.send(`<h1>Welcome to Hacker News! `)
-})
+    res.send(`
+    <div>
+        <h1>Welcome to Hacker News! </h1>
+        <a href="/puppies"> Go to Hacker News</a>
+    </div>
+    `)
+});
+
+// Example Route
+app.get("/puppies:puppyid", (request, response, next)=> {
+    res.send(`<h1> `)
+});
 
 // Step 6: Boot up Express server by "listening" to itconst PORT = 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`We are cooking on port ${PORT}`)
-})
+});
